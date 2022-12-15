5dea4a62a289dc206e6ae4229e7d2785542c85fd
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-12-15 21:21:58 +0530

    Retrive Top Records

---
+++ Product_Review_Management/Management.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Data;
+
+namespace Product_Review_Management
+{
+    public class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+        /// <summary>
+        /// Tops the records.
+        /// </summary>
+        /// <param name="review">The list.</param>
+        public void TopRecords(List<ProductReview> review)
+        {
+            var recordData = (from productReviews in review orderby productReviews.Rating descending select productReviews).Take(3);
+            Console.WriteLine("************************** Top Records **************************\n");
+            foreach (ProductReview product in recordData)
+            {
+                Console.WriteLine("ProductID : " + product.ProductID + " UserID : " + product.UserID + " Rating : " + product.Rating + " Review : " + product.Review + " IsLike : " + product.IsLike);
+            }
+        }
+    }
+}
--- Product_Review_Management/Program.cs
+++ Product_Review_Management/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Product review management problem statement");
+            Console.WriteLine("Welcome to Product review management problem statement\n");
 
             List<ProductReview> productReviewList = new List<ProductReview>()
             {
@@ -19,13 +19,30 @@
                 new ProductReview() { ProductID = 12, UserID = 10, Rating = 4, Review = "nice", IsLike = true },
                 new ProductReview() { ProductID = 13, UserID = 10, Rating = 4, Review = "nice", IsLike = true },
                 new ProductReview() { ProductID = 14, UserID = 10, Rating = 4, Review = "nice", IsLike = true },
-                new ProductReview() { ProductID = 15, UserID = 10, Rating = 4, Review = "nice", IsLike = true }
+                new ProductReview() { ProductID = 15, UserID = 10, Rating = 4, Review = "nice", IsLike = false },
+                new ProductReview() { ProductID = 16, UserID = 11, Rating = 5, Review = "nice", IsLike = true },
+                new ProductReview() { ProductID = 16, UserID = 11, Rating = 4.3, Review = "Good", IsLike = true },
+                new ProductReview() { ProductID = 17, UserID = 11, Rating = 4.3, Review = "nice", IsLike = false },
+                new ProductReview() { ProductID = 18, UserID = 12, Rating = 4.5, Review = "Bad", IsLike = true },
+                new ProductReview() { ProductID = 17, UserID = 13, Rating = 4.1, Review = "Good", IsLike = true },
+                new ProductReview() { ProductID = 18, UserID = 14, Rating = 3.2, Review = "nice", IsLike = false },
+                new ProductReview() { ProductID = 19, UserID = 15, Rating = 2.5, Review = "Bad", IsLike = true },
+                new ProductReview() { ProductID = 20, UserID = 15, Rating = 3.5, Review = "Good", IsLike = true },
+                new ProductReview() { ProductID = 21, UserID = 16, Rating = 4.8, Review = "Bad", IsLike = false },
+                new ProductReview() { ProductID = 22, UserID = 17, Rating = 4.5, Review = "Good", IsLike = true },
+                new ProductReview() { ProductID = 23, UserID = 18, Rating = 4.4, Review = "Bad", IsLike = true },
+                new ProductReview() { ProductID = 25, UserID = 19, Rating = 2.8, Review = "Good", IsLike = false },
+                new ProductReview() { ProductID = 23, UserID = 17, Rating = 3, Review = "Bad", IsLike = true },
 
             };
             foreach (ProductReview product in productReviewList)
             {
                 Console.WriteLine("ProductID : " + product.ProductID + " UserID : " + product.UserID + " Rating : " + product.Rating + " Review : " + product.Review + " IsLike : " + product.IsLike);
             }
+            Console.WriteLine();
+
+            Management management= new Management();
+            management.TopRecords(productReviewList);
         }
     }
 }
